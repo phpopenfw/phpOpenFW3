9bf7fc64ee4f6eb48e583fa4c1eb517a10fc8998
Christian Clark <chris@swelldevelopment.com>
2020-09-17 13:23:17 -0400

    Added GetAllHeaders method back to HTTP Helper class.

--- src/Helpers/HTTP.php
+++ src/Helpers/HTTP.php
@@ -50,4 +50,33 @@ class HTTP
 		exit;
 	}
 
+	//=========================================================================
+	//=========================================================================
+	/**
+	 * Return All HTTP Request Headers
+	 */
+	//=========================================================================
+	//=========================================================================
+	public static function GetAllHeaders()
+	{
+        if (function_exists('getallheaders')) {
+            return getallheaders();
+        }
+
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+           if (substr($name, 0, 5) == 'HTTP_') {
+               $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+               $headers[$name] = $value;
+           }
+           else if ($name == "CONTENT_TYPE") {
+               $headers["Content-Type"] = $value;
+           }
+           else if ($name == "CONTENT_LENGTH") {
+               $headers["Content-Length"] = $value;
+           }
+       }
+       return $headers;
+    }
+
 }
