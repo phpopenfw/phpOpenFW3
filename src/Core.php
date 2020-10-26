a5af2ffd3bd6dfbc32e37bda1edef1521307119b
Christian Clark <cclark61@gmail.com>
2020-10-26 19:28:12 -0400

    Added bootstrap check to load config and load data sources methods in Core.

--- src/Core.php
+++ src/Core.php
@@ -4,10 +4,10 @@
 /**
  * Framework Core Class
  *
- * @package		phpOpenFW
- * @author 		Christian J. Clark
- * @copyright	Copyright (c) Christian J. Clark
- * @license		https://mit-license.org
+ * @package        phpOpenFW
+ * @author         Christian J. Clark
+ * @copyright    Copyright (c) Christian J. Clark
+ * @license        https://mit-license.org
  **/
 //*****************************************************************************
 //*****************************************************************************
@@ -21,64 +21,64 @@ namespace phpOpenFW;
 //*****************************************************************************
 class Core
 {
-	//*************************************************************************
-	//*************************************************************************
+    //*************************************************************************
+    //*************************************************************************
     /**
      * Bootstrap Method
      */
-	//*************************************************************************
-	//*************************************************************************
+    //*************************************************************************
+    //*************************************************************************
     public static function Bootstrap($file_path=false, Array $args=[])
     {
-		//=====================================================================
+        //=====================================================================
         // Default Args / Extract Args
-		//=====================================================================
-		$load_config = false;
-		$config_file = false;
-		$load_data_sources = false;
-		$db_config_file = false;
-		$display_errors = false;
-		$config_index = 'config';
-		extract($args);
+        //=====================================================================
+        $load_config = false;
+        $config_file = false;
+        $load_data_sources = false;
+        $db_config_file = false;
+        $display_errors = false;
+        $config_index = 'config';
+        extract($args);
         
-		//=====================================================================
-		// Define Framework Path?
-		//=====================================================================
-	    if (!defined('PHPOPENFW_FRAME_PATH')) {
-	        $frame_path = realpath(__DIR__ . '/../../');
-	        define('PHPOPENFW_FRAME_PATH', $frame_path);
-	        $_SESSION['frame_path'] = PHPOPENFW_FRAME_PATH;
-		}
+        //=====================================================================
+        // Define Framework Path?
+        //=====================================================================
+        if (!defined('PHPOPENFW_FRAME_PATH')) {
+            $frame_path = realpath(__DIR__ . '/../../');
+            define('PHPOPENFW_FRAME_PATH', $frame_path);
+            $_SESSION['frame_path'] = PHPOPENFW_FRAME_PATH;
+        }
 
-		//=====================================================================
-		// Define File Path?
-		//=====================================================================
-		if (!defined('PHPOPENFW_APP_FILE_PATH')) {
-			if ($file_path && !is_dir($file_path)) {
-				trigger_error('Invalid file path given to Core Bootstrap method.');
-				return false;
-			}
-			define('PHPOPENFW_APP_FILE_PATH', $file_path);
-			$_SESSION['file_path'] = PHPOPENFW_APP_FILE_PATH;
-		}
+        //=====================================================================
+        // Define File Path?
+        //=====================================================================
+        if (!defined('PHPOPENFW_APP_FILE_PATH')) {
+            if ($file_path && !is_dir($file_path)) {
+                trigger_error('Invalid file path given to Core Bootstrap method.');
+                return false;
+            }
+            define('PHPOPENFW_APP_FILE_PATH', $file_path);
+            $_SESSION['file_path'] = PHPOPENFW_APP_FILE_PATH;
+        }
 
-		//=====================================================================
-		// Setup Methods
-		//=====================================================================
+        //=====================================================================
+        // Setup Methods
+        //=====================================================================
         self::set_version();
         self::detect_env();
 
-		//=====================================================================
+        //=====================================================================
         // Configuration Session Index
-		//=====================================================================
-		if (!$config_index) {
-    		$config_index = 'config';
-		}
+        //=====================================================================
+        if (!$config_index) {
+            $config_index = 'config';
+        }
         define('PHPOPENFW_CONFIG_INDEX', $config_index);
 
-		//=====================================================================
+        //=====================================================================
         // Load Configuration?
-		//=====================================================================
+        //=====================================================================
         if ($load_config) {
             self::LoadConfiguration([
                 'config_file' => $config_file,
@@ -86,9 +86,9 @@ class Core
             ]);
         }
 
-		//=====================================================================
+        //=====================================================================
         // Load Data Sources?
-		//=====================================================================
+        //=====================================================================
         if ($load_data_sources) {
             self::LoadDataSources([
                 'config_file' => $db_config_file,
@@ -96,9 +96,9 @@ class Core
             ]);
         }
 
-		//=====================================================================
+        //=====================================================================
         // Flag phpOpenFW as Bootstrapped
-		//=====================================================================
+        //=====================================================================
         define('PHPOPENFW_BOOTSTRAPPED', true);
     }
 
@@ -141,17 +141,22 @@ class Core
     //*************************************************************************
     public static function LoadConfiguration(Array $args=[])
     {
-    	//=====================================================================
+        //=====================================================================
+        // Check that phpOpenFW has been bootstrapped
+        //=====================================================================
+        \phpOpenFW\Core::CheckBootstrapped();
+
+        //=====================================================================
         // Defaults / Extract Args
-    	//=====================================================================
-    	$config_file = false;
+        //=====================================================================
+        $config_file = false;
         $session_index = 'config';
         $display_errors = false;
         extract($args);
 
-    	//=====================================================================
+        //=====================================================================
         // Configuration File Set?
-    	//=====================================================================
+        //=====================================================================
         if (!$config_file || !file_exists($config_file)) {
             $config_file = PHPOPENFW_APP_FILE_PATH . '/config.inc.php';
         }
@@ -173,7 +178,7 @@ class Core
         }
         else if ($display_errors) {
             trigger_error('Error: Configuration file does not exist.');
-		}
+        }
     }
 
     //*************************************************************************
@@ -186,50 +191,55 @@ class Core
     //*************************************************************************
     public static function LoadDataSources(Array $args=[])
     {
-    	//=====================================================================
+        //=====================================================================
+        // Check that phpOpenFW has been bootstrapped
+        //=====================================================================
+        \phpOpenFW\Core::CheckBootstrapped();
+
+        //=====================================================================
         // Defaults / Extract Args
-    	//=====================================================================
-    	$config_file = false;
+        //=====================================================================
+        $config_file = false;
         $force_reload = false;
         $display_errors = false;
         extract($args);
 
-    	//=====================================================================
-    	// Load Data Sources?
-    	//=====================================================================
-    	if ($force_reload || !defined('PHPOPENFW_DB_CONFIG_SET')) {
+        //=====================================================================
+        // Load Data Sources?
+        //=====================================================================
+        if ($force_reload || !defined('PHPOPENFW_DB_CONFIG_SET')) {
             if (!$config_file || !file_exists($config_file)) {
                 $config_file = PHPOPENFW_APP_FILE_PATH . '/config/data_sources.php';
             }
-    		if (file_exists($config_file)) {
-    			$data_arr = array();
-    			require($config_file);
+            if (file_exists($config_file)) {
+                $data_arr = array();
+                require($config_file);
 
-    			if (isset($data_arr) && !isset($data_sources)) {
-        			$data_sources = $data_arr;
-    			}
-
-    			if (!empty($data_sources) && is_array($data_sources)) {
-    				$key_arr = array_keys($data_sources);
-    				foreach ($key_arr as $key) {
-    					$reg_code = Core\DataSources::Register($key, $data_sources[$key]);
-    				}
-    				if (!defined('PHPOPENFW_DB_CONFIG_SET')) {
-        				define('PHPOPENFW_DB_CONFIG_SET', true);
-                    }
-    			}
-    			else {
-    				if ($display_errors) {
-        				trigger_error('Error: No data sources defined.');
-                    }
-    			}
-    		}
-    		else {
-        		if ($display_errors) {
-        			trigger_error('Error: Data Source Configuration file does not exist.');
+                if (isset($data_arr) && !isset($data_sources)) {
+                    $data_sources = $data_arr;
                 }
-    		}
-    	}
+
+                if (!empty($data_sources) && is_array($data_sources)) {
+                    $key_arr = array_keys($data_sources);
+                    foreach ($key_arr as $key) {
+                        $reg_code = Core\DataSources::Register($key, $data_sources[$key]);
+                    }
+                    if (!defined('PHPOPENFW_DB_CONFIG_SET')) {
+                        define('PHPOPENFW_DB_CONFIG_SET', true);
+                    }
+                }
+                else {
+                    if ($display_errors) {
+                        trigger_error('Error: No data sources defined.');
+                    }
+                }
+            }
+            else {
+                if ($display_errors) {
+                    trigger_error('Error: Data Source Configuration file does not exist.');
+                }
+            }
+        }
     }
 
     //*************************************************************************
@@ -266,22 +276,22 @@ class Core
     //*************************************************************************
     public static function session_kill()
     {
-    	if (isset($_SESSION)) {
-    		$_SESSION = array();
+        if (isset($_SESSION)) {
+            $_SESSION = array();
 
-    		if (ini_get('session.use_cookies')) {
-    		    $params = session_get_cookie_params();
-    		    setcookie(session_name(), '', time() - 42000,
-    		        $params['path'], $params['domain'],
-    		        $params['secure'], $params['httponly']
-    		    );
-    		}
+            if (ini_get('session.use_cookies')) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params['path'], $params['domain'],
+                    $params['secure'], $params['httponly']
+                );
+            }
 
-			session_unset();
-    		session_destroy();
-    		return true;
-    	}
-    	return false;
+            session_unset();
+            session_destroy();
+            return true;
+        }
+        return false;
     }
 
     //*************************************************************************
@@ -348,15 +358,15 @@ class Core
             $version = $_SESSION['PHPOPENFW_VERSION'];
         }
         else {
-        	$version = false;
-        	$ver_file = PHPOPENFW_FRAME_PATH . '/VERSION';
-        	if (file_exists($ver_file)) {
-        		$version = trim(file_get_contents($ver_file));
-        	}
-        	$_SESSION['PHPOPENFW_VERSION'] = $version;
+            $version = false;
+            $ver_file = PHPOPENFW_FRAME_PATH . '/VERSION';
+            if (file_exists($ver_file)) {
+                $version = trim(file_get_contents($ver_file));
+            }
+            $_SESSION['PHPOPENFW_VERSION'] = $version;
         }
-    	define('PHPOPENFW_VERSION', $version);
-    	return PHPOPENFW_VERSION;
+        define('PHPOPENFW_VERSION', $version);
+        return PHPOPENFW_VERSION;
     }
 
 }
